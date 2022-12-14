73db0fe61b46f9a1f71020917fb4fa32299c0662
Alex Meyers <meyersam0427@gmail.com>
2022-12-13 21:35:33 -0600

    simple arrays

---
+++ arrays.rb
@@ -0,0 +1,60 @@
+#1
+words = ['hello', 'how', 'you']
+words << 'alex'
+words << 'great'
+p words
+
+#2
+
+array = ['a', 'b', 'c', 'd']
+array[1] = 34
+p array
+
+
+#3
+
+array = [1,2,3,4,5]
+index = 0
+while index < array.length
+  p array[index]
+  index += 1
+end
+
+#4
+array = [1]
+array << 2
+array << 3
+array << 4
+p array
+
+#5
+array = ['a', 'b', 'c']
+array[2] = array[2].upcase
+p array
+
+#6
+names = ['alex', 'cody', 'chris']
+index = 0 
+while index < names.length
+  index += 1
+end
+puts names
+
+#7
+string = ['hello', 'world', 'become']
+string << 'amazing'
+p string
+
+array = [1,2,3,4,5]
+array[0] = array[0] * 10
+p array
+
+#8
+
+array = [1,2]
+index = 0 
+while index < array.length
+  index +=1
+end
+puts array
+
