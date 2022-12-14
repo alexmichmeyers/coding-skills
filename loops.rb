badc79bc2fc50ae0514d77d80fd1bea938b03298
Alex Meyers <meyersam0427@gmail.com>
2022-12-13 21:12:20 -0600

    basic loops

---
+++ loops.rb
@@ -0,0 +1,59 @@
+#1
+number = 1
+while number <=10 
+  p number
+  number +=1
+end
+
+#2
+word = 'hello'
+x = 1
+while x <=5
+  puts word
+  x += 1
+end
+
+#3
+
+
+#4
+number = 0 
+while number <= 100
+  puts number
+  number += 5
+end
+
+#5
+number = 9000
+x = 1
+while x <= 5
+  puts number
+  x += 1
+end
+
+#6
+number = 50
+while number <=70
+  puts number
+  number += 1
+end
+
+#7
+phrase = "Around the world"
+x = 1
+while x <= 10
+  puts phrase
+  x += 1
+end
+
+#8
+number = 2
+while number <= 40
+  puts number 
+  number += 2
+end
+
+
+
+
+
